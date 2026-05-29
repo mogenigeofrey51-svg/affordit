16433bec7da272cca94aca380754027db5074432
mogenigeofrey51-svg <mogenigeofrey51@gmail.com>
2026-05-29 09:04:18 +0300

    style: apply lifestyle home refresh stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import AfforditPrototype from "./App.jsx";
 import "./styles.css";
+import "./home-refresh.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
